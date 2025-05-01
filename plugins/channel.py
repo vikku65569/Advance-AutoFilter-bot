8fb1e1e7a8e4bb7cd76c836586078819483a7c66
tactition <zahidnazir330@gmail.com>
2025-05-02 00:24:44 +0530

    add the filter bot to repo

---
+++ plugins/channel.py
@@ -0,0 +1,13 @@
+
+
+from pyrogram import Client, filters
+from info import CHANNELS
+from database.ia_filterdb import save_file
+
+media_filter = filters.document | filters.video
+
+@Client.on_message(filters.chat(CHANNELS) & media_filter)
+async def media(bot, message):
+    media = getattr(message, message.media.value, None)
+    media.caption = message.caption
+    await save_file(media)
