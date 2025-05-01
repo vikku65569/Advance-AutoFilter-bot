ee97e2db3fae3bb0282661c0140d80089d064365
tactition <zahidnazir330@gmail.com>
2025-05-02 00:34:37 +0530

    add the filter bot to repo

--- plugins/Extra/engine.py
+++ plugins/Extra/engine.py
@@ -3,7 +3,7 @@
 import openai
 
 async def ai(query):
-    openai.api_key = "" #Your openai api key
+    openai.api_key = ""
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
      
