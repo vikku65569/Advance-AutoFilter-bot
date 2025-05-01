54c7956987ff3c76f4953d41706dd7fdffc6aa17
tactition <zahidnazir330@gmail.com>
2025-05-02 01:12:35 +0530

    removed bot.py send message

--- bot.py
+++ bot.py
@@ -45,8 +45,8 @@ async def start():
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
             print("Tech VJ Imported => " + plugin_name)
-    if ON_HEROKU:
-        asyncio.create_task(ping_server())
+    
+    asyncio.create_task(ping_server())
     b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
