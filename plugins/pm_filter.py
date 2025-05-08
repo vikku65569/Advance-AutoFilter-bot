573d2b48755d6512a05b6cc8059408c1403d517a
tactition <zahidnazir330@gmail.com>
2025-05-08 20:47:02 +0530

    working perfrectly fine final touch

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -12,7 +12,7 @@ from utils import get_size, is_subscribed, pub_is_subscribed, get_poster, search
 from database.users_chats_db import db
 from database.ia_filterdb import col, sec_col, db as vjdb, sec_db, get_file_details, get_search_results, get_bad_files
 from database.filters_mdb import del_all, find_filter, get_filters
-from database.connections_mdb import mydb, active_connection, all_connections, delete_connection, if_active, make_active, make_inactive,is_connected 
+from database.connections_mdb import mydb, active_connection, all_connections, delete_connection, if_active, make_active, make_inactive,connected_group
 from database.gfilters_mdb import find_gfilter, get_gfilters, del_allg
 from urllib.parse import quote_plus
 from Zahid.util.file_properties import get_name, get_hash, get_media_file_size
@@ -51,7 +51,7 @@ BUTTONS2 = {}
 SPELL_CHECK = {}
 
 
-@Client.on_message(filters.group & filters.text & filters.incoming & is_connected())
+@Client.on_message(filters.group & filters.text & filters.incoming & connected_group())
 async def give_filter(client, message):
     if message.chat.id != SUPPORT_CHAT_ID:
         settings = await get_settings(message.chat.id)
